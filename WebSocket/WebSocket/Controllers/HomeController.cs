75e2cdd6f0c05dc066d5e9a3944ccff577f58cd2
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-17 16:18:46 -0300

    inicio

---
+++ WebSocket/WebSocket/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace WebSocket.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpPost]
+        public async Task<ActionResult> HelloWorld()
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "u2",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+               "580199",
+               "78ade9e19befaaecf09c",
+               "daf7bc7d66d990f23172",
+                    options);
+
+            var result = await pusher.TriggerAsync("my-channel", "my-event", new { message = "Hello World"  });
+
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+
+    }
+}
---
+++ WebSocket/WebSocket/Controllers/SecondController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebSocket.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public class HelloWorldController : Controller
+        {
+            [HttpPost]
+            public async Task<ActionResult> HelloWorld()
+            {
+                var options = new PusherOptions
+                {
+                    Cluster = "us2",
+                    Encrypted = true
+                };
+
+                var pusher = new Pusher(
+                  "580199",
+                  "78ade9e19befaaecf09c",
+                  "daf7bc7d66d990f23172",
+                  options);
+
+                var result = await pusher.TriggerAsync(
+                  "my-channel",
+                  "my-event",
+                  new { message = "hello world" });
+
+                return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+            }
+        }
+    }
+}
